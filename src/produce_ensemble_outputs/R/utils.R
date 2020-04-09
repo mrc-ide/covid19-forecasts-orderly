f09a00c7396dc2cde228f490160a36e251a2571a
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2020-04-09 17:13:01 +0100

    Unweighted ensemble model

---
+++ src/produce_ensemble_outputs/R/utils.R
@@ -0,0 +1,72 @@
+pool_predictions <- function(outputs, weights = 1) {
+
+    wtd_outputs <- purrr::map2(outputs, weights, function(x, w) x * w)
+    out <- Reduce('rbind', wtd_outputs)
+    out
+
+}
+
+
+######################################################################
+## Extract model quantiles
+## Each model output is a list with several elements,
+## the key one is Predictions.
+## This is a list of data.frames with one data.frame for each country
+## For each country, we have a list of 2 components, corresponding
+## to the 2 serial intervals being considered.
+## it is this last list (country, 2 components) that is passed to
+## this function.
+extract_predictions_qntls <- function(y) {
+
+    names(y) <- paste0("si_", seq_along(y))
+    out <- purrr::map_dfr(
+        y,
+        function(y_si) {
+            out2 <- t(
+                apply(y_si,
+                      2,
+                      quantile,
+                      prob = c(0.025, 0.1, 0.4, 0.5, 0.6, 0.9, 0.975)
+                      )
+            )
+            out2 <- as.data.frame(out2)
+            out2 <- tibble::rownames_to_column(out2, var = "date")
+            out2
+
+        }, .id = "si"
+    )
+    out
+}
+
+
+daily_to_weekly <- function(y) {
+
+    names(y) <- paste0("si_", seq_along(y))
+    out <- purrr::map_dfr(
+        y,
+        function(y_si) {
+            weekly <- rowSums(y_si)
+            weekly <- quantile(
+                weekly,
+                prob = c(0.025, 0.1, 0.4, 0.5, 0.6, 0.9, 0.975)
+            )
+            weekly_df <- as.data.frame(weekly)
+            ## This is not the last date for which predictions are
+            ## available, but the last date for which observations are
+            ## available.
+            weekly_df$week_ending <- as.Date(colnames(y_si)[1]) - 1
+
+            weekly_df <- tibble::rownames_to_column(
+                weekly_df, var = "quantile"
+            )
+
+            weekly_df<- tidyr::spread(
+                weekly_df, key = quantile, value = weekly
+            )
+            weekly_df
+
+        }, .id = "si"
+    )
+    out
+
+}
---
+++ src/produce_ensemble_outputs/produce_ensemble_outputs.R
@@ -0,0 +1,130 @@
+weeks_ending <- list(
+  "2020-03-08" = "2020-03-08"
+  ## "2020-03-15" = "2020-03-15",
+  ## "2020-03-22" = "2020-03-22",
+  ## "2020-03-29" = "2020-03-29",
+  ## "2020-04-05" = "2020-04-05"
+)
+
+output_files <- list(
+  `RtI0_Std_results_week_end_2020-03-08` =
+    "RtI0_Std_results_week_end_2020-03-08.rds",
+  `sbkp_Std_results_week_end_2020-03-08` =
+    "sbkp_Std_results_week_end_2020-03-08.rds"
+)
+
+model_outputs <- purrr::map(output_files, readRDS)
+
+## For each, for each country, pool projections from diff models
+## In the output, the first level is week, 2nd is country and 3rd
+## is SI.
+ensemble_model_predictions <- purrr::map(
+  weeks_ending,
+  function(week) {
+    idx <- grep(x = names(model_outputs), pattern = week)
+    outputs <- purrr::map(model_outputs[idx], ~ .[["Predictions"]])
+    ## First Level is model, 2nd is country, 3rd is SI.
+    countries <- names(outputs[[1]])
+    names(countries) <- countries
+    purrr::map(
+      countries,
+      function(country) {
+        ## y is country specific output
+        y <- purrr::map(outputs, ~ .[[country]])
+        ## y has 2 components, one for each SI.
+        y_1 <- purrr::map(y, ~ .[[1]]) ## si_1
+        y_2 <- purrr::map(y, ~ .[[2]]) ## si_1
+
+        out <- list(
+          pool_predictions(y_1),
+          pool_predictions(y_2)
+        )
+      }
+    )
+  }
+)
+
+ensemble_model_rt <- purrr::map_dfr(
+  weeks_ending,
+  function(week) {
+    message("Week is ", week)
+    idx <- grep(x = names(model_outputs), pattern = week)
+    message("Working on models ", names(model_outputs)[idx])
+    outputs <- purrr::map(model_outputs[idx], ~ .[["R_last"]])
+    ## First Level is model, 2nd is country, 3rd is SI.
+    ## TODO pick countries from inout
+    countries <- names(outputs[[2]])
+    names(countries) <- countries
+    purrr::map_dfr(
+      countries,
+      function(country) {
+        ## y is country specific output
+        y <- purrr::map(outputs, ~ .[[country]])
+        ## y has 2 components, one for each SI.
+        ## Determine quantiles
+        probs <- c(0.025, 0.5, 0.975)
+        y_1 <- purrr::map(y, ~ .[[1]]) ## si_1
+        y_1 <- quantile(
+          unlist(y_1),
+          probs = probs
+        )
+        y_1 <- tibble::rownames_to_column(
+          data.frame(out2 = y_1),
+          var = "quantile"
+        )
+        y_1$si <- "si_1"
+
+        y_2 <- purrr::map(y, ~ .[[2]]) ## si_1
+        y_2 <- quantile(
+          unlist(y_2),
+          probs = probs
+        )
+        y_2 <- tibble::rownames_to_column(
+          data.frame(out2 = y_2),
+          var = "quantile"
+        )
+        y_2$si <- "si_2"
+        rbind(y_1, y_2)
+      },
+      .id = "country"
+    )
+  },
+  .id = "model" ## this is really week ending, but to be able to resue prev code, i am calling it model
+)
+
+ensemble_daily_qntls <- purrr::map_dfr(
+  ensemble_model_predictions,
+  function(pred) {
+    purrr::map_dfr(
+      pred, extract_predictions_qntls,
+      .id = "country"
+    )
+  },
+  .id = "proj"
+)
+
+ensemble_weekly_qntls <- purrr::map_dfr(
+  ensemble_model_predictions,
+  function(pred) {
+    purrr::map_dfr(
+      pred, daily_to_weekly,
+      .id = "country"
+    )
+  },
+  .id = "proj"
+)
+
+readr::write_rds(
+  x = ensemble_model_rt,
+  path = "ensemble_model_rt.rds"
+)
+
+readr::write_rds(
+  x = ensemble_daily_qntls,
+  path = "ensemble_daily_qntls.rds"
+)
+
+readr::write_rds(
+  x = ensemble_weekly_qntls,
+  path = "ensemble_weekly_qntls.rds"
+)
