5a385475541fd94a1708bc6219865979cf9f5694
sangeeta <sangeetabhatia03@gmail.com>
2020-04-12 17:44:31 +0100

    More complete now

--- src/produce_full_report/script.R
+++ src/produce_full_report/script.R
@@ -1,4 +1,6 @@
 rmarkdown::render(
   "produce_full_report.Rmd",
-  output_format = rmarkdown::html_document(self_contained = FALSE)
+  output_format = rmarkdown::html_document(
+    self_contained = FALSE, theme = "cerulean"
+  )
 )
