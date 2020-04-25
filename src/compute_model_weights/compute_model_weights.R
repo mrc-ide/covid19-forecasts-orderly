66a5e1cf6520e1bdabc765b1b39e83feaaf6e3e0
sangeeta <sangeetabhatia03@gmail.com>
2020-04-25 12:57:26 +0100

    Model weights computed

--- src/compute_model_weights/compute_model_weights.R
+++ src/compute_model_weights/compute_model_weights.R
@@ -1,35 +1,33 @@
+## df is a data.frame that has for a forecast period, for each
+## country, for each model, for each SI the rank of the model. Return
+## unnormalised weights for each forecast period and for each model.
+## That is, weights summed aross acc countries.
+## Normalise at the point at which they
+## are used to create ensemble.
+## rank is the column name that holds the rank - useful so that
+## we can switch rank to a different metric if needed.
+model_weights <- function(df) {
+  ## How many times was this model awarded each rank
+  out <- dplyr::count(df, model, rank_by_likelhd)
+  out$weight <- out$n * (1 / out$rank_by_likelhd)
+
+  dplyr::group_by(out, model) %>%
+    dplyr::summarise(weight = sum(weight)) %>%
+    dplyr::ungroup()
+}
+
 model_metrics <- readr::read_csv("model_predictions_error.csv")
 
-total_error <- dplyr::group_by(
-  model_metrics, model, forecast_date, country, si
-) %>%
-      dplyr::summarise(
-        total_rel_mae = sum(rel_mae),
-        total_rel_mse = sum(rel_mse)
-      ) %>% dplyr::ungroup()
 
-## Within each forecast period, how many countries did we produce
-## forecast for? Sanity check: Within each forecast period, this
-## number should be the same for all models. Except for SBSM
-## model which only projects for 12 countries.
-ncountries <- dplyr::group_by(
-  total_error, forecast_date, si, model) %>%
-  dplyr::summarise(ncountries = n()) %>%
-  dplyr::ungroup()
-
-## Sanity check
-# tidyr::spread(ncountries, model, ncountries)
-## For each country get the best model in each
-## forecast period.
-
+## For each country we have M models that have projections.
+## Rank these models according to a specified metric.
 x <- split(
-  total_error,
+  model_metrics,
   list(
-    total_error$forecast_date,
-    total_error$country,
-    total_error$si
-  ),
-  sep = "_"
+    model_metrics$forecast_date,
+    model_metrics$country,
+    model_metrics$si
+  )
 )
 
 x <- purrr::keep(x, ~ nrow(.) > 0)
@@ -37,53 +35,85 @@ x <- purrr::keep(x, ~ nrow(.) > 0)
 model_ranks <- purrr::map_dfr(
   x,
   function(df) {
-    df$rank_by_mae <- order(df$total_rel_mae)
-    df$rank_by_mse <- order(df$total_rel_mse)
+    df <- dplyr::group_by(df, forecast_date, model, country, si) %>%
+      dplyr::summarise_if(is.numeric, sum) %>%
+      dplyr::ungroup()
+
+    df$rank_by_relsq <- rank(df$rel_sq, )
+    df$rank_by_relabs <- rank(df$rel_abs)
+    ## The average likelihood is -ve, so flip the sign to get the
+    ## correct rank.
+    df$rank_by_likelhd <- rank(-df$avg_likelhd)
     df
   }
 )
 
-## For each forecast period, for how many countries
-## was a given model the "best" model.
-mae_rank_counts <- dplyr::group_by(model_ranks, forecast_date, si) %>%
-  dplyr::group_modify(~ dplyr::count(., model, rank_by_mae), keep = TRUE) %>%
-  dplyr::ungroup()
+## For each country, extract a list of models that were run for it.
+## Then group countries that have the same set of models
+## The SI doesn't matter here, because presumably a model that was run
+## using one serial interval was also run using the 2nd.
+model_ranks_by_period <- split(model_ranks, model_ranks$forecast_date)
 
-## ncountries from above tells us how many predicitons each model made.
-mae_rank_counts <- dplyr::left_join(mae_rank_counts, ncountries)
-
-mae_rank_1 <- dplyr::filter(mae_rank_counts, rank_by_mae == 1)
-
-
-mse_rank_counts <- dplyr::group_by(model_ranks, forecast_date, si) %>%
-  dplyr::group_modify(~ dplyr::count(., model, rank_by_mse), keep = TRUE) %>%
-  dplyr::ungroup()
-
-
-mse_rank_counts <- dplyr::left_join(mse_rank_counts, ncountries)
-
-mse_rank_1 <- dplyr::filter(mse_rank_counts, rank_by_mse == 1)
-
-## Model weights
-## These will be correct for the period that does not include
-## Sam's model because of the issue with different number of
-## countries.
-mae_rank_1$model_weights <- mae_rank_1$n / mae_rank_1$ncountries
-mse_rank_1$model_weights <- mse_rank_1$n / mse_rank_1$ncountries
-
-### Joining them with ncountries so that the model that never got
-### picked gets a weight of 0. Identified by NA in the row
-
-mae_model_weights <- dplyr::left_join(
-  dplyr::select(ncountries, forecast_date, si, model),
-  mae_rank_1
+countries_for_model <- purrr::map(
+  model_ranks_by_period,
+  function(df) {
+    nmodels_per_country <- dplyr::count(df, model, country) %>%
+      tidyr::spread(country, n, fill = 0)
+    countries <- colnames(nmodels_per_country)[-1]
+    names(countries) <- countries
+    ## A non-zero entry in row i, column j
+    ## nmodels_per_country indicates that model i was run for country j.
+    models_for_country <- purrr::map(
+      countries,
+      function(cntry) {
+        idx <- which(nmodels_per_country[[cntry]] > 0)
+        nmodels_per_country$model[idx]
+      }
+    )
+    models <- unique(models_for_country)
+    models <- purrr::map(models, ~ paste(., sep = "-", collapse = "-"))
+    names(models) <- models
+    purrr::map(
+      models,
+      function(model) {
+        out <- purrr::keep(
+          models_for_country, function(x) model == paste(x, sep = "-", collapse = "-")
+        )
+        names(out)
+      }
+    )
+  }
 )
 
-mse_model_weights <- dplyr::left_join(
-  dplyr::select(ncountries, forecast_date, si, model),
-  mse_rank_1
+### For each forecast period,
+### For each set of countries identified above, compute model
+### weights for each model.
+
+model_weights_per_group <- purrr::imap_dfr(
+  countries_for_model,
+  function(model_grp, period) {
+    purrr::map_dfr(
+      model_grp,
+      function(cntry_grp) {
+        df <- model_ranks[model_ranks$forecast_date == period, ]
+        df <- df[df$country %in% cntry_grp, ]
+        by_si <- split(df, df$si)
+        purrr::map_dfr(by_si, model_weights, .id = "si")
+      }, .id = "model_group"
+    )
+  }, .id = "forecast_date"
 )
 
+saveRDS(
+  object = model_weights_per_group,
+  file = "unnormalised_model_weights_per_group.rds"
+)
 
-readr::write_csv(x = mae_model_weights, path = "mae_model_weights.csv")
-readr::write_csv(x = mse_model_weights, path = "mse_model_weights.csv")
+saveRDS(
+  object = model_ranks,
+  file = "model_ranks.rds"
+)
+saveRDS(
+  object = countries_for_model,
+  file = "countries_grouped_by_models.rds"
+)
