bbddf3a5328ce5662a20beb31d3d6a8050d05365
sangeeta <sangeetabhatia03@gmail.com>
2020-06-03 13:29:44 +0100

    Model performance metrics

--- src/compare_ensemble_outputs/R/utils.R
+++ src/compare_ensemble_outputs/R/utils.R
@@ -38,7 +38,9 @@ all_forecasts <- function(obs, pred) {
       theme(legend.position = "none", legend.title = element_blank()) +
       xlab("") +
       ylab("") +
-      scale_x_date(limits = c(as.Date("2020-03-01"), NA)) +
+    scale_x_date(
+      limits = c(as.Date("2020-03-01"), NA), date_breaks = "2 weeks"
+    ) +
     facet_wrap(
       ~country,
       ncol = 1,
@@ -58,4 +60,25 @@ cap_predictions <- function(pred) {
     dplyr::mutate_if(y, is.numeric, ~ ifelse(.x > ymax, ymax, .x))
   }
  )
+}
+
+
+metrics_over_time <- function(df, si, countries, var, labels) {
+
+  df <- df[df$si == si, ]
+  df <- df[df$country %in% countries, ]
+  df <- tidyr::gather(df, var, val, `rel_mae`:`poisson_p`)
+  df <- df[df$var %in% var, ]
+
+  p <- ggplot(df, aes(date, val)) +
+    geom_point() +
+    facet_wrap(
+      ~var, scales = "free_y", ncol = 1, labeller = labeller(var = labels)
+    ) +
+    scale_x_date(date_breaks = "2 weeks") +
+    theme_classic() +
+    xlab("") +
+    ylab("")
+
+  p
 }
