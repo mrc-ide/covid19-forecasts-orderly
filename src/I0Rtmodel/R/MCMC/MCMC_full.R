419403e76c8565b3fdc256c134913b9df09418db
sangeeta <sangeetabhatia03@gmail.com>
2020-06-24 18:17:52 +0100

    Setting up RtI0 Model

--- src/I0Rtmodel/R/MCMC/MCMC_full.R
+++ src/I0Rtmodel/R/MCMC/MCMC_full.R
@@ -1,36 +1,40 @@
 #' MCMC iterate
 #'
-#' run the MCMC to sample posterior of R and initial coniditions at each location 
+#' run the MCMC to sample posterior of R and initial coniditions at each location
 #' FYI: this is called internally by adapt_tuning
-#' 
-#' @param I the incidence for the time window during which we assume Rt to be constant.  
+#'
+#' @param I the incidence for the time window during which we assume Rt to be constant.
 #'           I is a dataframe, first column are dates then incidence for all locations
 #'           nb of row is the size of time widows, dates must be sequential
-#' 
+#'
 #' @param N_geo integer of  numbers of locations
-#'                   
+#'
 #' @param iter integer, the number of iteration for the MCMC
 #'
 #' @param theta0 vector of inital parameters
 #'
-#' @param s variance of proposal distributions (log-normal) 
-#' 
+#' @param s variance of proposal distributions (log-normal)
+#'
 #' @param SI Serial interval distribution (see SI_gamma_dist_EpiEstim)
-#' 
-#' @param mu0: initial conidtions to guaranty that if R=1, then we predict the number of cases in the future will stablise at the mean number of cases observed in the time window
-#'              mu0 is also used as the mean of the (exponential) prior for intial conditions estimated
-#'              
+#'
+#' @param mu0: initial conidtions to guaranty that if R=1, then we
+#' predict the number of cases in the future will stablise at the
+#' mean number of cases observed in the time window
+#' mu0 is also used as the mean of the (exponential) prior for intial
+#' conditions estimated mean number of cases observed in the time
+#' window
+#'
 #' @param repli number of time the variance of the proposal is tuned (10 tends to be ok)
-#' 
+#'
 #' @param within_iter iterations for evaluate the accpetance with new proposal variances
-#' 
+#'
 #' @details  res a list containing 2 matrices: theta: matrix of posterior samples
 #'                      and logL: matrix of associated log-likelihood
 #' @export
-#' 
+#'
 
 MCMC_full <- function(I,N_geo,iter,theta0,s,SI,mu0,repli_adapt,within_iter, over_disp = NA){
- 
+
   res0 <- adapt_tuning(repli = repli_adapt,
                        I = I,
                        N_geo = N_geo,
@@ -38,18 +42,18 @@ MCMC_full <- function(I,N_geo,iter,theta0,s,SI,mu0,repli_adapt,within_iter, over
                        theta0 = theta0,
                        sigma = s,
                        SI = SI,
-                       mu0 = mu0, 
+                       mu0 = mu0,
                        over_disp = over_disp)
   # adaptative tuning bit: we run an MCMC with rep/10 iterations, then
   # adjust the proposal variance to reach 0.2
   # do again using parameter value from the last iteration of the previous MCMC
   # repeat 10 times
   # from experience, this is enough to tunes proposal variances well, but worth checking
-  # see below for final acceptance rate output 
+  # see below for final acceptance rate output
   # see Rscript/MCMC_Rt_2018.R for full function
-  
+
   # print('halfway!')             # message halfway through (effectively, including tuning, we do 2xrep iterations)
-  
+
   res <- MCMC_iter(incidence = I ,
                    N_geo = N_geo,
                    iter = iter,
@@ -62,7 +66,7 @@ MCMC_full <- function(I,N_geo,iter,theta0,s,SI,mu0,repli_adapt,within_iter, over
   # FYI: this is called internally by adapt_tuning
   # see Rscript/MCMC_Rt_2018.R for full function
   # needs:
-  # I: the incidence for the time window during which we assume Rt to be constant. 
+  # I: the incidence for the time window during which we assume Rt to be constant.
   # N_geo: the number of locations
   # iter: the number of iteration for the MCMC
   # theta0: inital parameters, here taken from the last MCMC iteration after tuning (save some burn-in)
@@ -70,8 +74,8 @@ MCMC_full <- function(I,N_geo,iter,theta0,s,SI,mu0,repli_adapt,within_iter, over
   # SI: the serial interval(use SI_gamma_dist_EpiEstim to define), or need to be a list with vector dist for the daily distribution and SItrunc: an integer for the threshold of serial interval, if SItrunc=40, then dist is 41 element long to include day 0
   # mu0: initial conidtions to guaranty that if R=1, then we predict the number of cases in the future will stablise at the mean number of cases observed in the time window
   # mu0 is also used as the mean of the (exponential) prior for intial conditions estimated
-  
- 
+
+
   return(res)
 }
 
--- src/I0Rtmodel/R/MCMC/MCMC_iter.R
+++ src/I0Rtmodel/R/MCMC/MCMC_iter.R
@@ -1,39 +1,39 @@
 #' MCMC iterate
 #'
-#' run the MCMC to sample posterior of R and initial coniditions at each location 
+#' run the MCMC to sample posterior of R and initial coniditions at each location
 #' FYI: this is called internally by adapt_tuning
-#' 
-#' @param incidence the incidence for the time window during which we assume Rt to be constant.  
+#'
+#' @param incidence the incidence for the time window during which we assume Rt to be constant.
 #'           I is a dataframe, first column are dates then incidence for all locations
 #'           nb of row is the size of time widows, dates must be sequential
-#' 
+#'
 #' @param N_geo integer of  numbers of locations
-#'                   
+#'
 #' @param iter integer, the number of iteration for the MCMC
 #'
 #' @param theta0 vector of inital parameters, here taken from the last MCMC iteration after tuning (save some burn-in)
 #'
 #' @param s variance of proposal distributions (log-normal) - tuned previously
-#' 
+#'
 #' @param SI Serial interval distribution (see SI_gamma_dist_EpiEstim)
-#' 
+#'
 #' @param mu0: initial conidtions to guaranty that if R=1, then we predict the number of cases in the future will stablise at the mean number of cases observed in the time window
 #'              mu0 is also used as the mean of the (exponential) prior for intial conditions estimated
-#' 
+#'
 #' @details  res a list containing 2 matrices: theta: matrix of posterior samples
 #'                      and logL: matrix of associated log-likelihood
 #' @export
-#' 
+#'
 
 MCMC_iter <- function(incidence,N_geo,iter,theta0,s,SI,mu0,over_disp = NA){
-  
+
   I <- incidence[,-1]     # remove dates (everything needs to be consecutive days)
-  
+
   #############################################################################
   # for MCMC
   thetas <- matrix(NA,iter,N_geo*2)  # Rt's and initial conditions for each location
   L <- thetas                         # store likelihoods
-  
+
   #############################################################################
   # get the daily 'forces of infections'
   lambda <- lambda_fct(param = theta0 , I = t(I), N_l = N_geo ,
@@ -42,16 +42,16 @@ MCMC_iter <- function(incidence,N_geo,iter,theta0,s,SI,mu0,over_disp = NA){
   L1 <- Like1(lambda,t(I),theta0[1:N_geo])
 
   L[1,] <- rep(L1,2)
-  thetas[1,] <- theta0       
-  
+  thetas[1,] <- theta0
+
   #############################################################################
   # sampling
-  for (i in 2:iter){   
+  for (i in 2:iter){
     #print(i)
     for (j in 1:2){
       Ts <- theta0
       lambdaT <- lambda
-      
+
       J <- (j-1)*N_geo+(1:N_geo)
       # propose new parameter j
       if (j <= 1){
@@ -63,30 +63,32 @@ MCMC_iter <- function(incidence,N_geo,iter,theta0,s,SI,mu0,over_disp = NA){
       lambdaT <- lambda_fct(param = Ts , I = t(I), N_l = N_geo ,
                             ws = rev(SI$dist) , SItrunc = SI$SItrunc)
       # get the likelihood for proposae value
-      
+
       Lint <- Like1(lambdaT,t(I),Ts[1:N_geo])
 
-      #get the ratio with previous value of parameter and correct for porposal (and, only for initial conditions, prior distribution)
+      ## get the ratio with previous value of parameter and correct
+      ## for porposal (and, only for initial conditions,
+      ## prior distribution
       if (j <= 1){
         r <- exp(Lint-L1)*Ts[J]/theta0[J]
       }else{
         r <- exp(Lint-L1)   #*exp(1/mu0[j-N_geo]*(theta0[j]-Ts[j])) # with weak exponential prior
       }
-      
+
       # accept or reject
       for (h in 1:N_geo){
         if (runif(1,0,1) <= r[h]){
           theta0[J[h]] <- Ts[J[h]]  # if accept, keep new parameter value
           lambda[h,] <- lambdaT[h,]   # if accept, keep new force of infection
           L1[h] <- Lint[h]          # if accept, keep new lieklihood
-        } 
+        }
       }
       L[i,J] <- L1    # store final likelihood
     }
-    
+
     thetas[i,] <- theta0  # store final parameter values for this iteration
   }
-  
+
   ####
   res <- list(theta = thetas, logL = L)
   return(res)
