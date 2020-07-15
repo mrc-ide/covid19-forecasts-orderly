13952d127d030a33a0299ad408a9b2ee5baf7caf
sangeeta <sangeetabhatia03@gmail.com>
2020-07-15 10:39:58 +0100

    Automatically generate orderly.yml

---
+++ write_dependencies_combined_rt.R
@@ -0,0 +1,44 @@
+## Generate orderly.yml for collate_model_outputs
+x <- list(
+  script = "script.R",
+  artefacts = list(
+    data = list(
+    description = "Collated model outputs (quantiles)",
+    filenames = c("weekly_iqr.rds", "combined_rt_estimates.rds")
+  )
+ ),
+ packages = c("dplyr", "tidyr", "ggplot2", "purrr", "rincewind"),
+ sources =  "R/utils.R"
+)
+
+
+week_starting <- as.Date("2020-03-08")
+week_ending <- as.Date("2020-03-29")
+weeks_needed <- seq(
+  from = week_starting, to = week_ending, by = "7 days"
+)
+
+x$depends <- purrr::map(
+  weeks_needed,
+  function(week) {
+  y <- list(
+    produce_ensemble_outputs = list(
+      id = glue::glue("latest(parameter:week_ending == \"{week}\")"),
+      use =  "ensemble_model_rt_samples.rds"
+    )
+   )
+   infiles <- purrr::map(
+     y$produce_ensemble_outputs$use,
+     function(x) strsplit(x, split = ".", fixed = TRUE)[[1]][1]
+  )
+  names(y$produce_ensemble_outputs$use) <- glue::glue("{infiles}_{week}.rds")
+  y
+ }
+)
+
+
+
+
+con <- file("src/produce_combined_rt/orderly.yml", "w")
+yaml::write_yaml(x, con)
+close(con)
