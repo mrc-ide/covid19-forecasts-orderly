377b9bacdb9bab572f003a8b99d6fd9314bb9b88
sangeeta <sangeetabhatia03@gmail.com>
2020-08-11 19:36:58 +0100

    Compute CV for the week

--- src/produce_baseline_error/script.R
+++ src/produce_baseline_error/script.R
@@ -1,12 +1,14 @@
 week_starting <- as.Date(week_starting)
 week_ending <- as.Date(week_ending)
 
-weeks <- seq(from = week_starting + 1, to = week_ending, by = "7 days")
+weeks <- seq(from = week_starting, to = week_ending, by = "7 days")
 
 model_input <- readRDS("model_input.rds")
 model_input <- model_input[model_input$dates > week_starting &
                            model_input$dates <= week_ending, ]
 tall <- tidyr::gather(model_input, country, deaths, -dates)
+
+weekly_cv <- function(vec) sd(vec) / mean(vec)
 
 weekly <- split(tall, tall$country) %>%
   purrr::map_dfr(
@@ -16,14 +18,18 @@ weekly <- split(tall, tall$country) %>%
       )
       incid <- slider::slide_period_vec(
         x$deaths, x$dates, "week", sum, .origin = week_starting
+        )
+      cv <- slider::slide_period_vec(
+        x$deaths, x$dates, "week", weekly_cv, .origin = week_starting
       )
       data.frame(
         week_starting = dates,
         weekly_incid = incid,
+        weekly_cv = cv,
         country = x$country[1]
       )
   }, .id = "country"
-) %>% tidyr::spread(country, weekly_incid)
+) ##%>% tidyr::spread(country, weekly_incid)
 
 ### If our predicitons for a week were the average deaths in the last
 ### week, what would the error we make?
@@ -43,10 +49,11 @@ null_model_error <- purrr::map_dfr(
       weeks,
       function(week) {
         obs <- model_input[model_input$dates %in% week, country]
-        prev_week <- model_input[model_input$dates %in% week - 7, country]
+        prev_week <- model_input[model_input$dates %in% (week - 7), country]
         null_pred <- matrix(mean(prev_week), ncol = 10000, nrow = 7)
         baseline <- assessr::rel_mae(obs = obs, pred = null_pred)
         data.frame(
+          week_starting = week[1],
           dates = week,
           null_error = baseline,
           prev_week_avg = mean(prev_week)
