71df1443d4dd07d8318f23ee43ac50cd09f5cd5a
sangeeta <sangeetabhatia03@gmail.com>
2020-08-13 23:06:02 +0100

    WIP longer forecasts

--- src/produce_weighted_combined_rt/script.R
+++ src/produce_weighted_combined_rt/script.R
@@ -12,23 +12,33 @@ week_ending <- as.Date(week_ending)
 ## week_ending is the week for which we already have observation,
 ## so that we can minimise the error for this week.
 weeks <- seq(from = week_ending - 7, to = as.Date("2020-03-08"), by = "-7 days")
-betas <- seq(0, 5, by = 0.1)
+betas <- seq(0, 5, by = 1)
 combined_rts <- map(
   betas,
   function(beta) {
-    weights <- exp(-beta * seq_along(weeks))
+    weights <- rev(exp(-beta * seq_along(weeks)))
     weights <- weights / sum(weights)
 
     idx <- sample(
-      x = weeks, size = 100000, replace = TRUE, prob = weights
+      x = weeks, size = 10000, replace = TRUE, prob = weights
     )
-    ## TODO the weight on some weeks might be small enough that
-    ## they are not sampled at all. Add component with 0 in nsamples
-    ## so that the next bit can work.
-    nsamples <- data.frame(table(idx))
+    nsamples <- data.frame(table(idx), stringsAsFactors = FALSE)
+    ## nsamples$idx <- as.character(nsamples$idx)
+    ## if (
+    ##   ! all(
+    ##       as.character(weeks) %in% as.character(nsamples$idx)
+    ##     )
+    ## ) {
+    ##   missing <- which(! as.character(weeks) %in% as.character(nsamples$idx))
+    ##   row <- data.frame(
+    ##     idx = weeks[missing],
+    ##     Freq = 1
+    ##   )
+    ##   nsamples <- rbind(nsamples, row)
+    ## }
     message(nsamples)
     message(beta)
-    map(
+    map_dfr(
       weeks,
       function(week) {
         message(week)
@@ -37,9 +47,11 @@ combined_rts <- map(
         out <- split(df, df$country) %>%
           map_dfr(
             function(country) {
+              size <- nsamples$Freq[nsamples$idx == week]
+              if (length(size) == 0) return(NULL)
               idx <- sample(
                 nrow(country),
-                size = nsamples$Freq[nsamples$idx == week],
+                size = size,
                 replace = TRUE
               )
               country[idx, ]
@@ -51,6 +63,33 @@ combined_rts <- map(
   }
 )
 
+model_input <- readRDS(
+  "~/OneDrive - Imperial College London/covid19-short-term-forecasts/model_inputs/data_2020-03-22.rds"
+)
+deaths_to_use <- model_input$D_active_transmission
+si <- EpiEstim::discr_si(0:30, model_input$si_mean[2], model_input$si_std[2])
+projections <- map(
+  combined_rts,
+  function(rt) {
+    by_country <- split(rt, rt$country)
+    imap(
+      by_country,
+      function(rt_country, country) {
+        message(country)
+        obs <- deaths_to_use[deaths_to_use$dates <= week_ending - 7, c("dates", country)]
+        obs$deaths <- obs[[country]]
+        incid <- rincewind:::ts_to_incid(obs, "dates", "deaths")
+        out <- rerun(
+          10,
+          projections::project(incid, rt_country$si_2, si, model = "poisson")
+        )
+        do.call(what = 'cbind', args = out)
+      }
+    )
+  }
+)
 
 
-
+observed <- readRDS(
+  "~/OneDrive - Imperial College London/covid19-short-term-forecasts/model_inputs/data_2020-03-29.rds"
+)
