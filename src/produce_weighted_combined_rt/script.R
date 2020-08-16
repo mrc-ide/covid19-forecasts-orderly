11ff995e6c1d17ecca08bf083a13414089ec5423
sangeeta <sangeetabhatia03@gmail.com>
2020-08-16 16:55:20 +0100

    Infrastructure set-up plus other changes

--- src/produce_weighted_combined_rt/script.R
+++ src/produce_weighted_combined_rt/script.R
@@ -1,21 +1,55 @@
+##orderly::orderly_develop_start(parameters = list(week_ending = "2020-03-29", use_si = "si_2"))
+## infiles <- list.files(pattern = "*.rds")
+
 run_info <- orderly::orderly_run_info()
 infiles <- run_info$depends$as
 
+infiles <- grep("latest", infiles, invert = TRUE, value = TRUE)
 
-##orderly::orderly_develop_start(parameters = list(week_ending = "2020-03-29", use_si = "si_2"))
-## infiles <- list.files(pattern = "*.rds")
 names(infiles) <- gsub(
   pattern = "ensemble_model_rt_samples_", replacement = "", x = infiles
 ) %>% gsub(pattern = ".rds", replacement = "", x = .)
 
 rt_samples <- purrr::map(infiles, readRDS)
+observed <- readRDS("latest_deaths_wide_no_filter.rds")
+
 week_ending <- as.Date(week_ending)
+week_prev <- week_ending - 7
+
+model_input <- readRDS(
+  glue::glue("{dirname(covid_19_path)}/model_inputs/data_{week_prev}.rds")
+)
+deaths_to_use <- model_input$D_active_transmission
+si <- EpiEstim::discr_si(0:30, model_input$si_mean[2], model_input$si_std[2])
+
+## countries included in the week_prev week of analysis
+countries <- setNames(model_input$Country, model_input$Country)
+
+country_weeks <- purrr::map(
+  countries,
+  function(country) {
+    weeks <- map(
+      rt_samples, ~ .[.$country == country, "model"][1]
+    )
+    weeks <- as.Date(unlist(weeks, recursive = FALSE))
+    consecutive_weeks <- list()
+    prev_week <- week_ending
+    while (prev_week %in% weeks) {
+      message(country, " in ", prev_week)
+      consecutive_weeks <- append(consecutive_weeks, prev_week)
+      prev_week <- prev_week - 7
+    }
+    consecutive_weeks
+  }
+)
+
+country_weeks <- keep(country_weeks, ~ length(.) > 0)
 
 ## week_ending is the week for which we already have observation,
 ## so that we can minimise the error for this week.
-weeks <- seq(from = week_ending - 7, to = as.Date("2020-03-08"), by = "-7 days")
 betas <- seq(0, 5, by = 1)
 names(betas) <- betas
+
 combined_rts <- map(
   betas,
   function(beta) {
@@ -26,19 +60,6 @@ combined_rts <- map(
       x = weeks, size = 10000, replace = TRUE, prob = weights
     )
     nsamples <- data.frame(table(idx), stringsAsFactors = FALSE)
-    ## nsamples$idx <- as.character(nsamples$idx)
-    ## if (
-    ##   ! all(
-    ##       as.character(weeks) %in% as.character(nsamples$idx)
-    ##     )
-    ## ) {
-    ##   missing <- which(! as.character(weeks) %in% as.character(nsamples$idx))
-    ##   row <- data.frame(
-    ##     idx = weeks[missing],
-    ##     Freq = 1
-    ##   )
-    ##   nsamples <- rbind(nsamples, row)
-    ## }
     message(nsamples)
     message(beta)
     map_dfr(
@@ -66,11 +87,45 @@ combined_rts <- map(
   }
 )
 
-model_input <- readRDS(
-  "~/OneDrive - Imperial College London/covid19-short-term-forecasts/model_inputs/data_2020-03-22.rds"
+
+combined_rts <- map(
+  betas,
+  function(beta) {
+    weights <- rev(exp(-beta * seq_along(weeks)))
+    weights <- weights / sum(weights)
+
+    idx <- sample(
+      x = weeks, size = 10000, replace = TRUE, prob = weights
+    )
+    nsamples <- data.frame(table(idx), stringsAsFactors = FALSE)
+    message(nsamples)
+    message(beta)
+    map_dfr(
+      weeks,
+      function(week) {
+        message(week)
+        week <- as.character(week)
+        df <- rt_samples[[week]]
+        out <- split(df, df$country) %>%
+          map_dfr(
+            function(country) {
+              size <- nsamples$Freq[nsamples$idx == week]
+              if (length(size) == 0) return(NULL)
+              idx <- sample(
+                nrow(country),
+                size = size,
+                replace = TRUE
+              )
+              country[idx, ]
+            }
+        )
+        out
+      }
+    )
+  }
 )
-deaths_to_use <- model_input$D_active_transmission
-si <- EpiEstim::discr_si(0:30, model_input$si_mean[2], model_input$si_std[2])
+
+
 
 projections <- map(
   combined_rts,
@@ -95,9 +150,7 @@ projections <- map(
 
 
 
-observed <- readRDS(
-  "/Volumes/Sangeeta EHD/covid19-forecasts-orderly/archive/prepare_ecdc_data/20200810-133647-0cb97781/latest_deaths_wide_no_filter.rds"
-)
+
 
 
 countries <- setNames(
@@ -123,8 +176,9 @@ error <- map(
 per_country <- map_dfr(
   countries,
   function(country) {
+    message(country)
     ## For each country, the value of beta that gives smallest error
-    x <- map(error, ~ .[[country]])
+    x <- map_dbl(error, ~ .[[country]])
     y <- x[which.min(x)]
     data.frame(
       beta = as.numeric(names(y)),
