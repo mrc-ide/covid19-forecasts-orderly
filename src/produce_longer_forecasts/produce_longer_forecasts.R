d69fd153a64dd33587827f02b2bf7c73f8a64c82
sangeeta <sangeetabhatia03@gmail.com>
2020-08-25 20:41:52 +0100

    Kind of works

--- src/produce_longer_forecasts/produce_longer_forecasts.R
+++ src/produce_longer_forecasts/produce_longer_forecasts.R
@@ -118,7 +118,7 @@ force_of_infection <- function(deaths, ws, R) {
 ## r_eff = r_obs / p_susceptible
 ## p_susceptible is the proportion susceptible at the point at which
 ## we start projecting ahead.
-project_with_saturation <- function(deaths, r_eff, p_susceptible, si, n_sim = 100, n_days, cfr) {
+project_with_saturation <- function(deaths, r_eff, p_susceptible, si, n_sim = 100, n_days, cfr, pop) {
 
   deaths_obs <- sum(deaths)
   I0 <- matrix(
@@ -131,6 +131,7 @@ project_with_saturation <- function(deaths, r_eff, p_susceptible, si, n_sim = 10
     p_s = vector(mode = "list", length = n_days)
   )
   for (day in 1:n_days) {
+    message(day)
     R <- r_eff * p_susceptible
     R <- sample(R, n_sim)
     ws <- tail(rev(si), length(deaths) + day - 1)
@@ -138,6 +139,8 @@ project_with_saturation <- function(deaths, r_eff, p_susceptible, si, n_sim = 10
       ws, ncol = 1,  nrow = length(deaths) + day - 1
     )
     lambda <- force_of_infection(I0, ws, R)
+    tmp <- quantile(lambda)
+    message(paste(tmp, collapse = " "))
     pred <- map_int(lambda, function(x) rpois(1, x))
     I0 <- cbind(I0, pred)
     deaths_so_far <- rowSums(I0)
@@ -152,32 +155,34 @@ project_with_saturation <- function(deaths, r_eff, p_susceptible, si, n_sim = 10
   out
 }
 
-unwtd_projections <- purrr::map(
+unwtd_projections <- map(
   countries,
   function(country) {
     message(country)
+    pop <- population$pop_data2018[population$countries_and_territories == country]
     n_days <- length(
       unwtd_rt_estimates[[country]]$weeks_combined
     ) * 7
-    x <- tall_deaths[[country]]
+    x <- deaths_to_use[[country]]
     ##rt <- unwtd_rt_estimates[[country]]$rt_samples
-    rt <- unwtd_reff[[country]][[]]
+    rt <- unwtd_reff[[country]][["r_eff"]]
+    p <- unwtd_reff[[country]][["p_susceptible"]]
     out <- purrr::map(
       seq_len(sims_per_rt),
       function(i) {
-        projections::project(
-          x = x,
-          R = rt,
+        project_with_saturation(
+          deaths = x,
+          r_eff = rt,
+          p_susceptible = p,
           si = si,
           n_sim = n_sim,
           n_days = n_days,
-          R_fix_within = TRUE,
-          model = "poisson"
-        ) %>%
-          as.matrix() %>% t
+          cfr = cfr_samples,
+          pop
+        )
       }
     )
-    do.call(what = "rbind", args = out)
+    ##do.call(what = "rbind", args = out)
   }
 )
 
@@ -205,9 +210,9 @@ wtd_across_all_projections <- purrr::map(
           as.matrix() %>% t
       }
     )
-    do.call(what = "rbind", args = out)
+    ##do.call(what = "rbind", args = out)
   }
-  )
+)
 
 wtd_per_country_projections <- purrr::map(
   countries,
