bd14e64be4348234e8af7043ec82bbfd5b701a97
sangeeta <sangeetabhatia03@gmail.com>
2020-10-05 14:34:40 +0100

    Changes for latest report

--- src/prepare_ecdc_data/prepare_ecdc_data.R
+++ src/prepare_ecdc_data/prepare_ecdc_data.R
@@ -296,6 +296,13 @@ raw_data$Deaths[raw_data$`Countries.and.territories` == "Israel" & raw_data$Date
 raw_data$Deaths[raw_data$`Countries.and.territories` == "Israel" & raw_data$DateRep == "2020-09-14"] <- 16
 raw_data$Deaths[raw_data$`Countries.and.territories` == "Israel" & raw_data$DateRep == "2020-09-13"] <- 13
 
+## Source: https://www.lanacion.com.ar/sociedad/coronavirus-buenos-aires-cargo-casi-3200-muertos-nid2466920
+## Argentina added 2997 deaths to its total
+raw_data$Deaths[raw_data$`Countries.and.territories` == "Argentina" & raw_data$DateRep == "2020-10-03"] <- 354
+
+
+
+
 raw_data <- split(raw_data, raw_data$`Countries.and.territories`) %>%
   map_dfr(
     function(df) {
@@ -364,12 +371,15 @@ deaths_to_use <- dplyr::rename(by_country_deaths, dates = "DateRep")
 Country <- colnames(deaths_to_use)[!colnames(deaths_to_use) == "dates"]
 
 exclude <- c(
+  "Angola",
   "Kazakhstan",
   "Oman",
   "United_States_of_America",
   "Syria",
   "Zimbabwe",
-  "Spain",
+  "Israel",
+  "Kenya",
+  "Tunisia",
   "Kosovo"
 )
 saveRDS(exclude, "exclude.rds")
--- src/produce_retrospective_vis/script.R
+++ src/produce_retrospective_vis/script.R
@@ -1,4 +1,4 @@
-week_ending <- as.Date("2020-09-27")
+week_ending <- as.Date(week_ending)
 palette <- c(
   Africa = "#d21d24",
   `North America` = "#5adf79",
