5bc851846f738a5df3c6dd66d578b0551e4351bb
sangeeta <sangeetabhatia03@gmail.com>
2020-10-09 19:25:17 +0100

    More figure tweaks and code DRYing up

--- src/produce_performance_metrics_vis/R/utils.R
+++ src/produce_performance_metrics_vis/R/utils.R
@@ -36,7 +36,7 @@ prop_in_cri_heatmap <- function(df, CrI = "50%") {
       breaks = unique(df$forecast_date),
       labels = unique(df$x_labels)
     ) +
-    theme_classic() +
+    theme_minimal() +
     scale_fill_distiller(
       palette = "Greens",
       direction = 1,
@@ -45,38 +45,59 @@ prop_in_cri_heatmap <- function(df, CrI = "50%") {
       limits = c(0, 1),
       name = glue("Proportion in {CrI} CrI")
     ) +
-  theme(
-    axis.text.x.bottom = element_text(
-      angle = 90, hjust = 0.5, vjust = 0.5, size = 6
-    ),
-    axis.text.y = element_text(size = 6),
-    plot.margin = margin(t = 30, r = 20, b = 0, l = 0),
-    legend.position = "bottom",
-    axis.line.x = element_blank()
-  ) +
-  xlab("") +
-  ylab("") +
-  coord_cartesian(clip = "off")
+    theme(
+      axis.line = element_blank(),
+      axis.text.x.bottom = element_text(
+        angle = 90, hjust = 0.5, vjust = 0.5, size = 6
+      ),
+      axis.text.y = element_text(size = 6),
+      plot.margin = margin(t = 30, r = 20, b = 0, l = 0),
+      legend.position = "bottom",
+      legend.key.width = unit(2, "lines"),
+      legend.key.height = unit(1, "lines")
+    ) +
+    xlab("") +
+    ylab("") +
+    coord_cartesian(clip = "off")
 
   p
 }
 
+augment_data <- function(df, width = 1.5) {
 
-relative_error_heatmap <- function(df) {
-
-  df$x_labels <- strftime(
-    as.Date(df$forecast_date), format = "%d-%b"
+  x <- data.frame(forecast_date = unique(df$forecast_date))
+  x$x <- seq(from = 1, by = width, length.out = nrow(x))
+  x_labels <- strftime(
+    as.Date(x$forecast_date), format = "%d-%b"
   )
-  df$forecast_date <- factor(df$forecast_date)
-  ymax <- max(as.integer(df$country)) + 1
-  xmax <- max(as.integer(df$forecast_date)) + 2.5
+  x_labels <- setNames(x_labels, x$x)
+
+  y <- data.frame(country = rev(levels(df$country)))
+  y$y <- seq(from = 1, by = width, length.out = nrow(y))
+
+  y_labels <- as.character(y$country) %>%
+    snakecase::to_title_case()
+  y_labels <- setNames(y_labels, y$y)
+
+  df <- left_join(df, x) %>% left_join(y)
+
+  list(
+    df = df, x_labels = x_labels, y_labels = y_labels
+  )
+
+}
+
+relative_error_heatmap <- function(df, x_labels, y_labels) {
+
+  ymax <- max(as.integer(df$y)) + 2
+  xmax <- max(as.integer(df$x)) + 3.5
 
   p <- ggplot() +
     geom_tile(
       data = df[df$rel_mae_mu < 2, ],
-      aes(forecast_date, country, fill = rel_mae_mu),
-      width = 0.9,
-      height = 0.8
+      aes(x, y, fill = rel_mae_mu),
+      width = 1.8,
+      height = 1.8
     ) +
   scale_fill_distiller(
     palette = "Spectral", na.value = "white", direction = -1,
@@ -90,35 +111,36 @@ relative_error_heatmap <- function(df) {
   ggnewscale::new_scale_fill() +
   geom_tile(
     data = df[df$rel_mae_mu >= 2, ],
-    aes(forecast_date, country),
-    fill = "#b2b2ff",
-    width = 0.9,
-    height = 0.8
+    aes(x, y),
+    fill = "#b2b2ff", width = 1.8, height = 1.8
   ) +
   geom_text(
     data = df,
-    aes(x = xmax, y = country, label = right_label),
+    aes(x = xmax, y = y, label = right_label),
     parse = TRUE, size = 2
   ) +
   geom_text(
     data = df,
-    aes(x = forecast_date, y = ymax, label = top_label),
+    aes(x = x, y = ymax, label = top_label),
     parse = TRUE, angle = 90, hjust = 0, vjust = 0, size = 2
   ) +
-  geom_text(
+  geom_richtext(
     data = df,
-    aes(x = forecast_date, y = country, label = cell_label),
-    size = 1.8, parse = TRUE
+    aes(x = x, y = y, label = cell_label), size = 1.7,
+      fontface = "bold", fill = NA, label.color = NA, # remove background and outline
+      label.padding = grid::unit(rep(0, 4), "pt") # remove padding
   ) +
-  scale_x_discrete(
-    breaks = unique(df$forecast_date),
-    labels = unique(df$x_labels)
+  scale_y_continuous(
+    breaks = sort(unique(df$y)),
+    labels = y_labels,
+    minor_breaks = NULL
   ) +
-  scale_y_discrete(
-    limits = rev(levels(df$country)),
-    labels = nice_country_name
+  scale_x_continuous(
+    breaks = sort(unique(df$x)),
+    labels = x_labels,
+    minor_breaks = NULL
   ) +
-  theme_classic() +
+  theme_minimal() +
   theme(
     axis.text.x.bottom = element_text(
       angle = 90, hjust = 0.5, vjust = 0.5, size = 6
@@ -126,7 +148,9 @@ relative_error_heatmap <- function(df) {
     axis.text.y = element_text(size = 6),
     plot.margin = margin(t = 30, r = 20, b = 0, l = 0),
     legend.position = "bottom",
-    axis.line.x = element_blank()
+    legend.key.width = unit(2, "lines"),
+    legend.key.height = unit(1, "lines"),
+    axis.line = element_blank()
   ) +
   xlab("") +
   ylab("") +
