b19cb933fd4e482aef52dd5e36b7e73773e74651
sangeeta <sangeetabhatia03@gmail.com>
2020-10-12 21:56:02 +0100

    Set up comparison of weekly and long-term Rt

--- src/collate_combined_rt/collate_combined_rt.R
+++ src/collate_combined_rt/collate_combined_rt.R
@@ -28,7 +28,7 @@ combined_rt_qntls <- f(infiles, "combined_rt_estimates")
 saveRDS(combined_rt_qntls, "combined_rt_qntls.rds")
 
 
-combined_wtd_rt_estimates <- f(
+combined_wtd_rt_qntls <- f(
   infiles, "combined_weighted_estimates_across_countries"
 )
 
@@ -38,7 +38,7 @@ saveRDS(
 )
 
 
-combined_wtd2_rt_estimates <- f(
+combined_wtd2_rt_qntls <- f(
   infiles, "combined_weighted_estimates_per_country"
 )
 
@@ -55,78 +55,3 @@ weekly_iqr <- map_dfr(
 )
 
 saveRDS(weekly_iqr, "weekly_iqr.rds")
-
-weekly_iqr <- split(weekly_iqr, weekly_iqr$country)
-
-plots <- imap(
-  weekly_iqr,
-  function(y, country) {
-    message(country)
-    x <- dplyr::distinct(weekly_iqr[[country]])
-    x <- split(x, x$forecast_week) %>%
-      map_dfr(
-        function(df) {
-          df <- df[rep(seq_len(nrow(df)), each = 7), ]
-          df$dates <- seq(
-            from = df$forecast_week[1],
-            length.out = 7,
-            by = "1 day"
-          )
-          df
-        }
-    )
-    cntry <- map(combined_rt_estimates, ~ .[[country]])
-    cntry <- keep(cntry, ~ ! is.null(.))
-    cntry_combined <- map_dfr(
-      cntry, function(y) {
-        df <- data.frame(
-          week_starting1 = min(y$weeks_combined),
-          week_starting2 = max(y$weeks_combined),
-          `2.5%` = y$combined_rt[["2.5%"]],
-          `50%` = y$combined_rt[["50%"]],
-          `97.5%` = y$combined_rt[["97.5%"]],
-          check.names = FALSE
-    )
-    ## Extend this for the days between week_starting2 and
-    ## week_starting1 + another 7 days because you would have continued
-    ## with Rt estimate for another 7 days
-    z <- as.Date(y$weeks_combined)
-    ndays <- as.numeric(max(z) - min(z))
-
-    df <- df[rep(seq_len(nrow(df)), each = ndays), ]
-    df$dates <- seq(
-      from = df$week_starting1[1],
-      length.out = ndays,
-      by = "1 day"
-    )
-        df
-
-      }
-    )
-
-    ymax <- ceiling(max(x[["97.5%"]]))
-
-    p1 <- plot_weekly_iqr(x) +
-      coord_cartesian(clip = "off") +
-      scale_x_date(date_breaks = "2 week") +
-      ylim(0, ymax)
-
-    xmin <- min(x$forecast_week)
-    xmax <-max(x$forecast_week)
-    cntry_combined$forecast_week <- cntry_combined$week_starting2
-    p2 <- plot_combined_iqr(cntry_combined) +
-      coord_cartesian(clip = "off") +
-      ylim(0, ymax) +
-      scale_x_date(date_breaks = "2 week", limits = c(xmin, xmax))
-
-    p <- cowplot::plot_grid(
-      p1, p2, ncol = 1, align = "hv", rel_heights = c(0.7, 1)
-    )
-    p
-  }
-)
-
-iwalk(
-  plots,
-  function(p, country) ggsave(glue::glue("figures/{country}.png"), p)
-)
---
+++ src/compare_r_effective/compare_r_effective.R
@@ -0,0 +1,145 @@
+## orderly::orderly_develop_start(use_draft = "newer")
+dir.create("figures")
+reff_overlaps_weekly <- function(reff, weekly) {
+  x <- left_join(
+    reff, weekly, by = c("date" = "dates"),
+    suffix = c("_reff", "_weekly")
+  )
+  x <- na.omit(x)
+
+  out <- select(
+    x,
+    reff_low = `2.5%_reff`, reff_high = `97.5%_reff`,
+    weekly_low = `2.5%_weekly`, weekly_high = `97.5%_weekly`
+  ) %>%
+    pmap_lgl(
+      function(reff_low, reff_high, weekly_low, weekly_high) {
+        rincewind::overlaps(x1 = c(reff_low, reff_high),
+                            x2 = c(weekly_low, weekly_high),
+                            digits = 3
+                            )
+      }
+    )
+  data.frame(day = seq_along(out), reff_overlaps_weekly = out)
+}
+
+infiles <- list(
+  unweighted = "unweighted_reff_qntls.rds",
+  weighted_across_countries = "weighted_across_countries_reff_qntls.rds",
+  weighted_per_country = "weighted_per_country_reff_qntls.rds"
+)
+
+reff_by_strategy <- map(infiles, readRDS)
+
+## We assume transmissibility remains constant for 1 week.
+## Therefore, for each country, for each week, repeat the rows for 7
+## days and increment dates
+weekly_iqr <- readRDS("weekly_iqr.rds")
+weekly_iqr_augm <- split(weekly_iqr, weekly_iqr$country) %>%
+  map(function(x) {
+    message(x$country[1])
+    x <- dplyr::distinct(x)
+    x <- split(x, x$forecast_week) %>%
+      map_dfr(
+        function(df) {
+          df <- df[rep(seq_len(nrow(df)), each = 7), ]
+          df$dates <- seq(
+            from = df$forecast_week[1] + 1,
+            length.out = 7,
+            by = "1 day"
+          )
+        df
+      }
+     )
+    x
+  }
+)
+
+
+overlap <- map(
+  reff_by_strategy,
+  function(strategy) {
+    countries <- setNames(names(strategy), names(strategy))
+    map_dfr(
+      countries,
+      function(country) {
+        weekly <- weekly_iqr_augm[[country]]
+        reff <- strategy[[country]]
+        reff$date <- as.Date(reff$date)
+        reff_overlaps_weekly(reff, weekly)
+      }, .id = "country"
+    )
+  }
+)
+
+iwalk(
+  reff_by_strategy,
+  function(strategy, nm) {
+    countries <- setNames(names(strategy), names(strategy))
+    walk(
+      countries,
+      function(country) {
+        weekly <- weekly_iqr_augm[[country]]
+        reff <- strategy[[country]]
+        reff$date <- as.Date(reff$date)
+        p <- ggplot() +
+          geom_ribbon(
+            data = reff, aes(date, ymin = `2.5%`, ymax = `97.5%`), alpha = 0.2
+          ) +
+          geom_line(data = reff, aes(date, `50%`)) +
+          geom_ribbon(
+            data = weekly,
+            aes(dates, ymin = `2.5%`, ymax = `97.5%`, group = forecast_week),
+            alpha = 0.2, fill = "blue"
+          ) +
+          geom_line(
+            data = weekly, aes(dates, `50%`, group = forecast_week)
+          ) +
+          geom_hline(yintercept = 1, linetype = "dashed") +
+          scale_x_date(date_breaks = "2 weeks") +
+          ylab("Reproduction number") +
+          xlab("") +
+          theme_minimal() +
+          ggtitle(
+            glue("Effective and weekly reproduction number for {country}")
+          )
+        outfile <- glue("figures/{nm}_{country}.png")
+        ggsave(filename = outfile, p)
+      }
+    )
+  }
+)
+
+iwalk(
+  overlap,
+  function(df, nm) {
+    ntrue <- filter(df, reff_overlaps_weekly == "TRUE") %>%
+      count(country, reff_overlaps_weekly, sort = TRUE)
+
+    df$country <- factor(
+      df$country, levels = ntrue$country, ordered = TRUE
+    )
+    ## We forecast ahead for 56 days, hence x axis goes from 1 to 56
+    df$day <- factor(df$day, levels = 1:56, ordered = TRUE)
+    nice_country_name <- function(x) snakecase::to_title_case(as.character(x))
+
+    p <- ggplot(df) +
+      geom_tile(
+        aes(day, country, fill = reff_overlaps_weekly),
+        width = 0.9, height = 0.9
+      ) +
+      theme_minimal() +
+      scale_y_discrete(
+        limits = rev(levels(df$country)),
+        labels = nice_country_name
+      ) +
+      theme(
+        legend.position = "bottom", legend.title = element_blank()
+      ) +
+      ylab("") +
+      xlab("")
+    outfile <- glue("figures/{nm}_overall.png")
+    ggsave(filename = outfile, p)
+  }
+)
+
