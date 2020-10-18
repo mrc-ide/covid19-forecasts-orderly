31e7ca5f63859a980cf0147c3bdd89494be8335e
sangeeta <sangeetabhatia03@gmail.com>
2020-10-18 18:53:30 +0100

    EDA and separation of concerns

--- src/produce_longer_forecasts_perf_viz/longer_forecasts_perf_viz.R
+++ src/produce_longer_forecasts_perf_viz/longer_forecasts_perf_viz.R
@@ -1,2 +1,52 @@
 daily <- readRDS("long_projections_error_daily.rds")
 weekly <- readRDS("long_projections_error_weekly.rds")
+
+by_week_proj <- group_by(weekly, week_of_projection) %>%
+  summarise_if(is.numeric, mean) %>%
+  ungroup()
+
+by_country <- group_by(weekly, country, week_of_projection) %>%
+  summarise_if(is.numeric, mean) %>%
+  ungroup()
+
+
+ggplot() +
+  geom_tile(
+    data = by_country[by_country$rel_mae <= 5, ],
+    aes(week_of_projection, country, fill = rel_mae)
+  ) +
+  scale_fill_distiller(
+    palette = "Spectral", na.value = "white", direction = -1,
+    guide = guide_colourbar(
+      title = "Relative Error",
+      title.position = "left",
+      title.vjust = 0.5,
+      order = 1
+    )
+  ) +
+  ggnewscale::new_scale_fill() +
+  geom_tile(
+    data = by_country[by_country$rel_mae > 5 & by_country$rel_mae < 10, ],
+    aes(week_of_projection, country, fill = rel_mae)
+  ) +
+  scale_fill_distiller(
+    palette = "YlOrRd", na.value = "white", direction = 1,
+    guide = guide_colourbar(
+      title = "2 < Model Error <= 5",
+      title.position = "top",
+      title.hjust = 0.5,
+      order = 2
+    )
+  ) +
+  theme_minimal()
+
+nice_country_name <- function(x) snakecase::to_title_case(as.character(x))
+
+
+long_relative_error_heatmap(by_country)
+
+quantile()
+
+ggplot(by_country) +
+  geom_histogram(aes(rel_mae), binwidth = 0.1) +
+  facet_wrap(~week_of_projection, ncol = 2, scales = "free_x")
