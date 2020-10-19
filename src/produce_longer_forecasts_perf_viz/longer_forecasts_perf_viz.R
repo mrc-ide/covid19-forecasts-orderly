3605c0176a145f3a8f9f34c996b766cc9152f477
sangeeta <sangeetabhatia03@gmail.com>
2020-10-19 22:16:13 +0100

    Relative error heat-map

--- src/produce_longer_forecasts_perf_viz/longer_forecasts_perf_viz.R
+++ src/produce_longer_forecasts_perf_viz/longer_forecasts_perf_viz.R
@@ -1,15 +1,17 @@
 ## orderly::orderly_develop_start(use_draft = "newer")
 daily <- readRDS("long_projections_error_daily.rds")
 weekly <- group_by(daily, strategy, country, week_of_projection) %>%
-  summarise_if(is.numeric, list(mu = mean, sd = sd))
+  summarise_if(is.numeric, mean)
+
+better_than_null <- readRDS("better_than_null.rds")
 
 weekly$country <- factor(
-    weekly$country,
-    levels = better_than_null$country, ordered = TRUE
-  )
+  weekly$country, levels = better_than_null$country, ordered = TRUE
+)
 
+weekly$week_of_projection <- factor(weekly$week_of_projection)
 
-weekly_incid <- readRDS("weekly_incidence.rds")
+country_groups <- readRDS("country_groups.rds")
 
 compare <- ggplot(
   weekly, aes(factor(week_of_projection), rel_mae, fill = strategy)
@@ -28,13 +30,6 @@ cbbPalette <- c(
 
 names(cbbPalette) <- 1:8
 
-weekly$week_of_projection <- factor(weekly$week_of_projection)
-
-f <- scales::trans_breaks("log10", function(x) 10^x)
-g <- scales::trans_format("log10", scales::math_format(10^.x))
-breaks <- union(f(weekly$obs_mu), f(weekly$rel_mae_mu))
-labels <- g(breaks)
-
 p <- ggplot(
   weekly,
   aes(log(obs_mu, 10), log(rel_mae_mu, 10), col = week_of_projection)
@@ -50,69 +45,15 @@ p <- ggplot(
 
 ggsave("incid_vs_relative_error.png", p)
 
-country_groups <- readRDS("country_groups.rds")
+
 
 by_strategy <- split(weekly, weekly$strategy)
 
-by_strategy_week <- map(
-  by_strategy,
-  function(df) {
-    group_by(df, strategy, week_of_projection) %>%
-      summarise_if(is.numeric, list(mu = mean, sd = sd)) %>%
-      ungroup()
-  }
-)
-
-by_strategy_country <- map(
-  by_strategy,
-  function(df) {
-    group_by(df, strategy, country) %>%
-      summarise_if(is.numeric, list(mu = mean, sd = sd)) %>%
-      ungroup()
-  }
-)
-
 x <- by_strategy[[1]]
+
 df <- x[x$country %in% country_groups[[1]], ]
+df <- weekly_summary(df)
+
 out <- augment_data(df)
 
-ggplot() +
-  geom_tile(
-    data = by_country[by_country$rel_mae <= 5, ],
-    aes(week_of_projection, country, fill = rel_mae)
-  ) +
-  scale_fill_distiller(
-    palette = "Spectral", na.value = "white", direction = -1,
-    guide = guide_colourbar(
-      title = "Relative Error",
-      title.position = "left",
-      title.vjust = 0.5,
-      order = 1
-    )
-  ) +
-  ggnewscale::new_scale_fill() +
-  geom_tile(
-    data = by_country[by_country$rel_mae > 5 & by_country$rel_mae < 10, ],
-    aes(week_of_projection, country, fill = rel_mae)
-  ) +
-  scale_fill_distiller(
-    palette = "YlOrRd", na.value = "white", direction = 1,
-    guide = guide_colourbar(
-      title = "2 < Model Error <= 5",
-      title.position = "top",
-      title.hjust = 0.5,
-      order = 2
-    )
-  ) +
-  theme_minimal()
-
-nice_country_name <- function(x) snakecase::to_title_case(as.character(x))
-
-
-long_relative_error_heatmap(by_country)
-
-quantile()
-
-ggplot(by_country) +
-  geom_histogram(aes(rel_mae), binwidth = 0.1) +
-  facet_wrap(~week_of_projection, ncol = 2, scales = "free_x")
+df <- out[[1]]
