494b4d7a2b956cd6aab2031b638b68a02ea4dd3c
sangeeta <sangeetabhatia03@gmail.com>
2021-01-21 23:04:24 +0000

    Model 3 schematic

--- src/produce_schematic/script.R
+++ src/produce_schematic/script.R
@@ -1,6 +1,11 @@
-##orderly::orderly_develop_start(use_draft = "newer")
+## orderly::orderly_develop_start(use_draft = "newer")
 ## Random
 si <- rgamma(1e4, shape = 2.3, rate = 1.28)
+
+theme_schematic <- function() {
+  theme_classic() %+replace%
+    theme(axis.text = element_blank(), axis.ticks = element_blank())
+}
 
 psi <- ggplot() +
   geom_density(aes(si), fill = "red", col = NA, alpha = 0.2) +
@@ -28,16 +33,16 @@ obs_deaths <- obs_deaths[obs_deaths$dates >= as.Date("2020-03-31"), ]
 
 obs_deaths$seen <- ifelse(obs_deaths$dates < now_minus_tau, 0.3, 1)
 
-obs <- ggplot(obs_deaths, aes(dates, deaths, alpha = seen)) +
-  geom_line() +
+obs <- ggplot() +
+  geom_line(data = obs_deaths, aes(dates, deaths, alpha = seen)) +
   scale_x_date(
     limits = c(as.Date("2020-03-31"), as.Date("2020-07-31"))
   ) +
   scale_alpha_identity() +
   xlab("Time") +
   ylab("Daily Deaths") +
-  theme_classic() +
-  theme(axis.text = element_blank(), axis.ticks = element_blank())
+  theme_schematic()
+
 
 
 
@@ -83,3 +88,72 @@ obs_m1 <- obs +
   ) + coord_trans(clip = "off")
 
 
+## https://stackoverflow.com/questions/35633239/add-curly-braces-to-ggplot2-and-then-use-ggsave
+x <- seq(0, 1, 0.1)
+p <- ggplot() + geom_point(aes(x, x), alpha = 0) + theme_void()
+grid.brackets(52, 24, 49, 350, h = 0.1, lwd = 4)
+dev.copy(png, "paren.png", height = 1000, width = 1000, res = 200)
+dev.off()
+
+
+###### Model 1 jointly estimates Rt and incidence prior to window
+sim_deaths <- map_dfr(
+  1:1000,
+  function(i) {
+    df <- obs_deaths[obs_deaths$seen == 0.3, ]
+    df$deaths <- df$deaths + rnorm(nrow(df), 5, 5)
+    df
+  }, .id = "sim"
+)
+
+p <- ggplot() +
+  geom_line(
+    data = sim_deaths, aes(dates, deaths, group = sim),
+    linetype = "dashed", alpha = 0.1
+  )
+
+
+######################################################################
+######## Model 3: Death to Cases
+obs_cases <- model_outputs$I_active_transmission[ , c("dates", "Peru")]
+## Get a really smooth curve
+obs_cases$cases <- slide_dbl(
+  obs_cases$Peru, mean, .before = 5, .after = 5
+)
+obs_cases <- obs_cases[obs_cases$dates <= now, ]
+obs_cases <- obs_cases[obs_cases$dates >= as.Date("2020-03-31"), ]
+## To kind of bring cases and deaths on same scale
+obs_cases$cases <-  obs_cases$cases / 10
+
+
+obs <- ggplot() +
+  geom_line(data = obs_deaths, aes(dates, deaths)) +
+  geom_line(data = obs_cases, aes(dates, cases), col = "blue") +
+  geom_vline(xintercept = as.numeric(now), linetype = "dashed") +
+  geom_text(aes(x = now + 3, y = 190, label = "now"), size = 8 / .pt) +
+  geom_text(
+    aes(x = now_minus_tau - 6, y = 190, label = "now - tau"),
+    size = 8 / .pt
+  ) +
+  geom_text(
+    aes(x = as.Date("2020-07-10"), y = 375, label = "Cases"),
+    color = "blue"
+  ) +
+  geom_text(
+    aes(x = as.Date("2020-07-10"), y = 200, label = "Deaths")
+  ) +
+  scale_x_date(
+    limits = c(as.Date("2020-03-31"), as.Date("2020-07-31"))
+  ) +
+  xlab("Time") + ylab("cases / deaths") +
+  theme_schematic() +
+  theme(axis.title.y = element_blank())
+
+
+delay <- rgamma(1e4, shape = 4, scale = 2.5)
+pdelay <- ggplot() +
+  geom_density(aes(delay), fill = "black", col = NA, alpha = 0.2) +
+  geom_vline(xintercept = 7.5, linetype = "dashed") +
+  xlab("Days since case reported") +
+  ylab("Probability of death") +
+  theme_schematic()
