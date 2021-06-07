7da4b2208573f38926bbcd953543d52ea43b641c
sangeeta <sangeetabhatia03@gmail.com>
2021-06-07 09:40:59 +0100

    Latest data

---
+++ weekly-run.sh
@@ -0,0 +1,6 @@
+orderly run prepare_ecdc_data week_ending=2021-06-06
+orderly run run_rti0 short_run=FALSE week_ending=2021-06-06
+orderly run run_apeestim week_ending=2021-06-06
+orderly run DeCa_model week_ending=2021-06-06
+orderly run process_individual_models week_ending=2021-06-06
+orderly run produce_ensemble_outputs week_ending=2021-06-06
