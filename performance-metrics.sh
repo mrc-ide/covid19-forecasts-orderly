c8e3b083b51f836c7bc3ebea88af0dda1ab736f1
sangeeta <sangeetabhatia03@gmail.com>
2021-06-07 23:05:21 +0100

    Updated shell script

--- performance-metrics.sh
+++ performance-metrics.sh
@@ -1,258 +1,105 @@
+
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-03-08
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-03-08
  orderly run produce_performace_metrics window=1 week_ending=2020-03-08
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-03-15
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-03-15
  orderly run produce_performace_metrics window=1 week_ending=2020-03-15
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-03-22
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-03-22
  orderly run produce_performace_metrics window=1 week_ending=2020-03-22
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-03-29
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-03-29
  orderly run produce_performace_metrics window=1 week_ending=2020-03-29
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-04-05
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-04-05
  orderly run produce_performace_metrics window=1 week_ending=2020-04-05
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-04-12
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-04-12
  orderly run produce_performace_metrics window=1 week_ending=2020-04-12
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-04-19
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-04-19
  orderly run produce_performace_metrics window=1 week_ending=2020-04-19
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-04-26
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-04-26
  orderly run produce_performace_metrics window=1 week_ending=2020-04-26
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-05-03
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-05-03
  orderly run produce_performace_metrics window=1 week_ending=2020-05-03
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-05-10
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-05-10
  orderly run produce_performace_metrics window=1 week_ending=2020-05-10
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-05-17
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-05-17
  orderly run produce_performace_metrics window=1 week_ending=2020-05-17
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-05-24
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-05-24
  orderly run produce_performace_metrics window=1 week_ending=2020-05-24
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-05-31
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-05-31
  orderly run produce_performace_metrics window=1 week_ending=2020-05-31
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-06-07
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-06-07
  orderly run produce_performace_metrics window=1 week_ending=2020-06-07
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-06-14
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-06-14
  orderly run produce_performace_metrics window=1 week_ending=2020-06-14
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-06-21
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-06-21
  orderly run produce_performace_metrics window=1 week_ending=2020-06-21
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-06-28
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-06-28
  orderly run produce_performace_metrics window=1 week_ending=2020-06-28
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-07-05
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-07-05
  orderly run produce_performace_metrics window=1 week_ending=2020-07-05
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-07-12
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-07-12
  orderly run produce_performace_metrics window=1 week_ending=2020-07-12
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-07-19
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-07-19
  orderly run produce_performace_metrics window=1 week_ending=2020-07-19
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-07-26
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-07-26
  orderly run produce_performace_metrics window=1 week_ending=2020-07-26
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-08-02
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-08-02
  orderly run produce_performace_metrics window=1 week_ending=2020-08-02
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-08-09
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-08-09
  orderly run produce_performace_metrics window=1 week_ending=2020-08-09
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-08-16
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-08-16
  orderly run produce_performace_metrics window=1 week_ending=2020-08-16
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-08-23
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-08-23
  orderly run produce_performace_metrics window=1 week_ending=2020-08-23
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-08-30
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-08-30
  orderly run produce_performace_metrics window=1 week_ending=2020-08-30
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-09-06
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-09-06
  orderly run produce_performace_metrics window=1 week_ending=2020-09-06
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-09-13
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-09-13
  orderly run produce_performace_metrics window=1 week_ending=2020-09-13
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-09-20
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-09-20
  orderly run produce_performace_metrics window=1 week_ending=2020-09-20
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-09-27
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-09-27
  orderly run produce_performace_metrics window=1 week_ending=2020-09-27
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-10-04
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-10-04
  orderly run produce_performace_metrics window=1 week_ending=2020-10-04
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-10-11
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-10-11
  orderly run produce_performace_metrics window=1 week_ending=2020-10-11
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-10-18
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-10-18
  orderly run produce_performace_metrics window=1 week_ending=2020-10-18
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-10-25
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-10-25
  orderly run produce_performace_metrics window=1 week_ending=2020-10-25
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-11-01
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-11-01
  orderly run produce_performace_metrics window=1 week_ending=2020-11-01
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-11-08
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-11-08
  orderly run produce_performace_metrics window=1 week_ending=2020-11-08
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-11-15
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-11-15
  orderly run produce_performace_metrics window=1 week_ending=2020-11-15
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-11-22
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-11-22
  orderly run produce_performace_metrics window=1 week_ending=2020-11-22
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-11-29
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-11-29
  orderly run produce_performace_metrics window=1 week_ending=2020-11-29
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-12-06
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-12-06
  orderly run produce_performace_metrics window=1 week_ending=2020-12-06
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-12-13
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-12-13
  orderly run produce_performace_metrics window=1 week_ending=2020-12-13
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-12-20
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-12-20
  orderly run produce_performace_metrics window=1 week_ending=2020-12-20
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2020-12-27
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2020-12-27
  orderly run produce_performace_metrics window=1 week_ending=2020-12-27
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2021-01-03
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2021-01-03
  orderly run produce_performace_metrics window=1 week_ending=2021-01-03
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2021-01-10
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2021-01-10
  orderly run produce_performace_metrics window=1 week_ending=2021-01-10
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2021-01-17
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2021-01-17
  orderly run produce_performace_metrics window=1 week_ending=2021-01-17
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2021-01-24
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2021-01-24
  orderly run produce_performace_metrics window=1 week_ending=2021-01-24
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2021-01-31
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2021-01-31
  orderly run produce_performace_metrics window=1 week_ending=2021-01-31
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2021-02-07
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2021-02-07
  orderly run produce_performace_metrics window=1 week_ending=2021-02-07
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2021-02-14
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2021-02-14
  orderly run produce_performace_metrics window=1 week_ending=2021-02-14
  Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2021-02-21
- orderly run produce_performance_metrics_ensemble window=1 week_ending=2021-02-21
  orderly run produce_performace_metrics window=1 week_ending=2021-02-21
-
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18329
- orderly run produce_performace_metrics window=1 week_ending=18329
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18336
- orderly run produce_performace_metrics window=1 week_ending=18336
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18343
- orderly run produce_performace_metrics window=1 week_ending=18343
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18350
- orderly run produce_performace_metrics window=1 week_ending=18350
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18357
- orderly run produce_performace_metrics window=1 week_ending=18357
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18364
- orderly run produce_performace_metrics window=1 week_ending=18364
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18371
- orderly run produce_performace_metrics window=1 week_ending=18371
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18378
- orderly run produce_performace_metrics window=1 week_ending=18378
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18385
- orderly run produce_performace_metrics window=1 week_ending=18385
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18392
- orderly run produce_performace_metrics window=1 week_ending=18392
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18399
- orderly run produce_performace_metrics window=1 week_ending=18399
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18406
- orderly run produce_performace_metrics window=1 week_ending=18406
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18413
- orderly run produce_performace_metrics window=1 week_ending=18413
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18420
- orderly run produce_performace_metrics window=1 week_ending=18420
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18427
- orderly run produce_performace_metrics window=1 week_ending=18427
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18434
- orderly run produce_performace_metrics window=1 week_ending=18434
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18441
- orderly run produce_performace_metrics window=1 week_ending=18441
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18448
- orderly run produce_performace_metrics window=1 week_ending=18448
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18455
- orderly run produce_performace_metrics window=1 week_ending=18455
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18462
- orderly run produce_performace_metrics window=1 week_ending=18462
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18469
- orderly run produce_performace_metrics window=1 week_ending=18469
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18476
- orderly run produce_performace_metrics window=1 week_ending=18476
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18483
- orderly run produce_performace_metrics window=1 week_ending=18483
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18490
- orderly run produce_performace_metrics window=1 week_ending=18490
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18497
- orderly run produce_performace_metrics window=1 week_ending=18497
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18504
- orderly run produce_performace_metrics window=1 week_ending=18504
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18511
- orderly run produce_performace_metrics window=1 week_ending=18511
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18518
- orderly run produce_performace_metrics window=1 week_ending=18518
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18525
- orderly run produce_performace_metrics window=1 week_ending=18525
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18532
- orderly run produce_performace_metrics window=1 week_ending=18532
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18539
- orderly run produce_performace_metrics window=1 week_ending=18539
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18546
- orderly run produce_performace_metrics window=1 week_ending=18546
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18553
- orderly run produce_performace_metrics window=1 week_ending=18553
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18560
- orderly run produce_performace_metrics window=1 week_ending=18560
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18567
- orderly run produce_performace_metrics window=1 week_ending=18567
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18574
- orderly run produce_performace_metrics window=1 week_ending=18574
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18581
- orderly run produce_performace_metrics window=1 week_ending=18581
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18588
- orderly run produce_performace_metrics window=1 week_ending=18588
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18595
- orderly run produce_performace_metrics window=1 week_ending=18595
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18602
- orderly run produce_performace_metrics window=1 week_ending=18602
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18609
- orderly run produce_performace_metrics window=1 week_ending=18609
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18616
- orderly run produce_performace_metrics window=1 week_ending=18616
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18623
- orderly run produce_performace_metrics window=1 week_ending=18623
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18630
- orderly run produce_performace_metrics window=1 week_ending=18630
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18637
- orderly run produce_performace_metrics window=1 week_ending=18637
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18644
- orderly run produce_performace_metrics window=1 week_ending=18644
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18651
- orderly run produce_performace_metrics window=1 week_ending=18651
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18658
- orderly run produce_performace_metrics window=1 week_ending=18658
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18665
- orderly run produce_performace_metrics window=1 week_ending=18665
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18672
- orderly run produce_performace_metrics window=1 week_ending=18672
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18679
- orderly run produce_performace_metrics window=1 week_ending=18679
- Rscript orderly-helper-scripts/dependencies_weighted_performance.R 18686
- orderly run produce_performace_metrics window=1 week_ending=18686
+ Rscript orderly-helper-scripts/dependencies_weighted_performance.R 2021-02-28
+ orderly run produce_performace_metrics window=1 week_ending=2021-02-28
