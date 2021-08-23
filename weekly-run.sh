a813fded9914f3c0b20e6478c7b1f8277f431d04
sangeeta <sangeetabhatia03@gmail.com>
2021-08-23 14:36:14 +0100

    Scripts

--- weekly-run.sh
+++ weekly-run.sh
@@ -1,6 +1,6 @@
-orderly run prepare_ecdc_data week_ending=2021-08-15
-orderly run run_rti0 short_run=FALSE week_ending=2021-08-15
-orderly run run_apeestim week_ending=2021-08-15
-orderly run DeCa_model week_ending=2021-08-15
-orderly run process_individual_models week_ending=2021-08-15
-orderly run produce_ensemble_outputs week_ending=2021-08-15
+orderly run prepare_ecdc_data week_ending=2021-08-22
+orderly run run_rti0 short_run=FALSE week_ending=2021-08-22
+orderly run run_apeestim week_ending=2021-08-22
+orderly run DeCa_model week_ending=2021-08-22
+orderly run process_individual_models week_ending=2021-08-22
+orderly run produce_ensemble_outputs week_ending=2021-08-22
