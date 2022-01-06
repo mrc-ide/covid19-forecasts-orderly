2cc595027b0ac74155e861fb199bcd47c365a0b7
sangeeta <sangeetabhatia03@gmail.com>
2022-01-06 16:10:43 +0000

    EDA

--- src/compare_model_perf_metrics/script.R
+++ src/compare_model_perf_metrics/script.R
@@ -3,21 +3,25 @@ source("R/weekly_error_summary.R")
 better_than_null <- readRDS("better_than_null.rds")
 country_groups <- readRDS("country_groups.rds")
 
-indv_perf <- readr::read_csv("model_predictions_error.csv")
-indv_perf <- filter(indv_perf, si == "si_2")
-indv_perf <- rename(indv_perf, "forecast_date" = "model")
+model_perf <- read_csv("unwtd_pred_error.csv")
+model_perf <- filter(model_perf, si == "si_2")
+model_perf <- rename(model_perf, "forecast_date" = "model")
 
-weekly_summaries <- map(
-  country_groups,
-  function(countries) {
-    df <- indv_perf[indv_perf$country %in% countries, ]
-    df$country <- factor(df$country, levels = countries, ordered = TRUE)
-    out <- split(df, df$model_name) %>%
-      map_dfr(weekly_summary, .id = "model_name")
-    out
-  }
-)
+weekly_summaries <- group_by(
+  model_perf, model_name, country
+) %>%
+  summarise_if(is.numeric, list(mu = mean, sd = sd)) %>%
+  ungroup()
 
-ensb_weekly <- readr::read_csv("unwtd_pred_weekly_summary.csv")
+y <- tidyr::gather(weekly_summaries, var, val, mae_mu:obs_sd)
+y <- tidyr::spread(y, model_name, val)
+colnames(y)[3:6] <- c("ApeEstim", "DeCa", "Ensemble", "RtI0")
+y_metrics <- split(y, y$var)
+y_metrics <- map(y_metrics, function(x) {
+  x$best <- apply(x[, c(-1, -2)], 1, which.min)
+  x
+})
 
 
+out <- map_dfr(y_metrics, function(x) count(x, best), .id = "var")
+out <- tidyr::spread(out, best, n)
