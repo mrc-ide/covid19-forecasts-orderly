5b18a85ec4ac0b56bba0a2735509b15ec5abe63a
sangeeta <sangeetabhatia03@gmail.com>
2022-02-15 10:31:26 +0000

    Edits for the latest report

--- weekly-run.sh
+++ weekly-run.sh
@@ -4,3 +4,4 @@ orderly run run_apeestim week_ending=2022-02-13
 orderly run DeCa_model week_ending=2022-02-13
 orderly run process_individual_models week_ending=2022-02-13
 orderly run produce_ensemble_outputs week_ending=2022-02-13
+orderly run produce_retrospective_vis week_ending=2022-02-13
