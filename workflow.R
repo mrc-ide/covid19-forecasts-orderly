5a71f5473a66d2e9684dcc8bd2a83038ff08d6de
sangeeta <sangeetabhatia03@gmail.com>
2022-03-01 17:07:12 +0000

    Latest report

--- workflow.R
+++ workflow.R
@@ -102,10 +102,10 @@ report_workflow <- function(week, use_draft = "newer", commit = FALSE) {
   # then pull latest branch on server. And then...
   # orderly run produce_retrospective_vis week_ending=2021-12-05 (NB: put latest date here)
 
-  ##orderly_run(
-  orderly_pull_archive(
+  orderly_run(
+  ##orderly_pull_archive(
     "produce_retrospective_vis",
-    parameter = list(week_ending = week)
+    parameter = list(week_ending = week), use_draft = use_draft
   )
 
   if (commit) orderly_commit(a)
@@ -119,7 +119,7 @@ report_workflow <- function(week, use_draft = "newer", commit = FALSE) {
 
   a <- orderly_run(
     "produce_full_report", use_draft = use_draft,
-    parameter = list(week_ending = week, week_in_words = "21^st^ February 2022")
+    parameter = list(week_ending = week, week_in_words = "1^st^ March 2022")
   )
 
   if (commit) orderly_commit(a)
@@ -187,7 +187,7 @@ library(glue)
 library(orderly)
 source("orderly-helper-scripts/dependencies_weighted_performance.R")
 source("orderly-helper-scripts/dependencies_collated_outputs_viz.R")
-week <- "2022-02-20"
+week <- "2022-02-27"
 use_draft <- "newer"
 ## This is the sunday before the Monday for which we are producing
 ## the report.
