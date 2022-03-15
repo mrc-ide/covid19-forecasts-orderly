3b3f7336d0993a5430588813d6c7d7afd4960bb1
sangeeta <sangeetabhatia03@gmail.com>
2022-03-15 12:22:01 +0000

    Edits for latest report

--- src/prepare_hm_outputs/script.R
+++ src/prepare_hm_outputs/script.R
@@ -8,7 +8,7 @@ ecdc <- distinct(
   select(ecdc, `Countries and territories`, popData2018)
 )
 
-cases_tall <- tidyr::gather(cases, country, cases, -dates)
+cases_tall <- gather(cases, country, cases, -dates)
 cases_tall <- left_join(
   cases_tall, ecdc, by = c("country" = "Countries and territories")
 )
@@ -16,7 +16,7 @@ cases_tall$per_capita_cases <- cases_tall$cases / cases_tall$popData2018
 cases_tall$per_1e6_cases <- round(cases_tall$per_capita_cases * 100000)
 
 
-deaths_tall <- tidyr::gather(deaths, country, deaths, -dates)
+deaths_tall <- gather(deaths, country, deaths, -dates)
 deaths_tall <- left_join(
   deaths_tall, ecdc, by = c("country" = "Countries and territories")
 )
@@ -24,10 +24,15 @@ deaths_tall$per_capita_deaths <- deaths_tall$deaths / deaths_tall$popData2018
 deaths_tall$per_1e6_deaths <- round(deaths_tall$per_capita_deaths * 100000)
 
 cases_per_1e6 <- select(cases_tall, dates, country, per_1e6_cases) %>%
-  tidyr::spread(country, per_1e6_cases)
+  spread(country, per_1e6_cases)
 
 deaths_per_1e6 <- select(deaths_tall, dates, country, per_1e6_deaths) %>%
-  tidyr::spread(country, per_1e6_deaths)
+  spread(country, per_1e6_deaths)
+## Round everything
+cases <- mutate_if(cases, is.numeric, as.integer)
+deaths <- mutate_if(deaths, is.numeric, as.integer)
+cases_per_1e6 <- mutate_if(cases_per_1e6, is.numeric, as.integer)
+deaths_per_1e6 <- mutate_if(deaths_per_1e6, is.numeric, as.integer)
 
 write_csv(cases, "cases_all.csv")
 write_csv(deaths, "deaths_all.csv")
@@ -38,9 +43,11 @@ write_csv(deaths_per_1e6, "deaths_per_1e6.csv")
 
 ## outputs
 model_outputs <- readRDS("ensemble_model_predictions.rds")
+model_outputs <- mutate_if(model_outputs, is.numeric, as.integer)
 write_csv(model_outputs, "latest_model_outputs.csv")
 
 ## weekly
 weekly <- readRDS("ensemble_weekly_qntls.rds")
+weekly <- mutate_if(weekly, is.numeric, as.integer)
 write_csv(weekly, "weekly_projected_deaths.csv")
 file.create("deploy.txt")
