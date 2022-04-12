871197b50c49fad34da47b75e483a0305145eff1
j-wardle <j.wardle19@imperial.ac.uk>
2022-04-12 09:28:49 +0100

    latest report runs for server

--- weekly-run.sh
+++ weekly-run.sh
@@ -1,7 +1,7 @@
-orderly run prepare_ecdc_data week_ending=2022-04-03
-orderly run run_rti0 short_run=FALSE week_ending=2022-04-03
-orderly run run_apeestim week_ending=2022-04-03
-orderly run DeCa_model week_ending=2022-04-03
-orderly run process_individual_models week_ending=2022-04-03
-orderly run produce_ensemble_outputs week_ending=2022-04-03
-orderly run produce_retrospective_vis week_ending=2022-04-03
+orderly run prepare_ecdc_data week_ending=2022-04-10
+orderly run run_rti0 short_run=FALSE week_ending=2022-04-10
+orderly run run_apeestim week_ending=2022-04-10
+orderly run DeCa_model week_ending=2022-04-10
+orderly run process_individual_models week_ending=2022-04-10
+orderly run produce_ensemble_outputs week_ending=2022-04-10
+orderly run produce_retrospective_vis week_ending=2022-04-10
