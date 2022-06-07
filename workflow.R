a78e44450aae4ca37f025abe7e536ed2bb5f7fa2
sangeeta <sangeetabhatia03@gmail.com>
2022-06-07 12:14:54 +0100

    Edits for latest report

--- workflow.R
+++ workflow.R
@@ -187,7 +187,7 @@ library(glue)
 library(orderly)
 source("orderly-helper-scripts/dependencies_weighted_performance.R")
 source("orderly-helper-scripts/dependencies_collated_outputs_viz.R")
-week <- "2022-05-15"
+week <- "2022-06-05"
 use_draft <- "newer"
 ## This is the sunday before the Monday for which we are producing
 ## the report.
