7124546ecc9f3b2124629beafc0c89614035d666
Jack Wardle <j.wardle19@imperial.ac.uk>
2022-08-04 17:27:33 +0100

    reconstruct incidence for individual forecast dates

--- src/reconstruct_daily_incidence/reconstruct_incidence.R
+++ src/reconstruct_daily_incidence/reconstruct_incidence.R
@@ -8,38 +8,86 @@ deaths_to_use <- model_input$D_active_transmission
 si_distrs <- readRDS("si_distrs.rds")
 
 # Select incidence from time when Florida reported daily
-# use Feb 2021 (plus the preceding two weeks)
+# use March 2021 as the forecasted weeks
+# for each forecast recreate the daily incidence for the previous 6 weeks
+# the first two weeks don't get used because they can vary in quality,
+# then we use ~1 month of data in the forecast models
 
-start <- "2021-01-18"
-end <- "2021-02-28"
-analysis_period <- seq.Date(from = as.Date(start),
-                            to = as.Date(end),
-                            by = 1)
+forecast_weeks <- c("2021-02-28", "2021-03-07", "2021-03-14",
+                    "2021-03-21")
 
-incid <- deaths_to_use[deaths_to_use$dates %in% analysis_period, c("dates", location)]
-incid$week_starting <- cut(incid$dates, "week")
-incid <- incid %>% 
-  group_by(week_starting) %>% 
-  mutate(weekly_incidence = sum(Florida))
+weekly_dat <- map(forecast_weeks, function(week) {
+  
+  end <- as.Date(week)
+  start <- end - 41 # select 6 weeks of data
+  analysis_period <- seq.Date(from = as.Date(start),
+                              to = as.Date(end),
+                              by = 1)
+  
+  incid <- deaths_to_use[deaths_to_use$dates %in% analysis_period, c("dates", location)]
+  incid$week_starting <- cut(incid$dates, "week")
+  incid <- incid %>% 
+    group_by(week_starting) %>% 
+    mutate(weekly_incidence = sum(Florida))
+  
+  aggr_incid <- incid %>%
+    group_by(week_starting) %>% 
+    summarise(weekly_incidence = mean(weekly_incidence))
+  
+  #convert to vector
+  out <- pull(aggr_incid, weekly_incidence)
+  out
+  
+})
 
-aggr_incid <- incid %>%
-  group_by(week_starting) %>% 
-  summarise(weekly_incidence = mean(weekly_incidence))
-
-#convert to vector
-weekly_dat <- pull(aggr_incid, weekly_incidence)
+names(weekly_dat) <- forecast_weeks
 
 # define si mean and sd
 config <- make_config(list(mean_si = 4.80,
                            std_si = 2.70))
 
-res <- estimate_R_agg(incid = weekly_dat,
-                      dt = 7, # aggregation window
-                      dt_out = 7, # length of sliding window used
-                      iter = 10,
-                      config = config, 
-                      method = "parametric_si")
 
-res
+# use EpiEstim to get the reconstructed incidence
 
-incid$reconstructed_incid <- res$I
+recon_daily_inc <- map(weekly_dat, function(w) {
+  
+  res <- estimate_R_agg(incid = w,
+                        dt = 7, # aggregation window
+                        dt_out = 7, # length of sliding window used
+                        iter = 10,
+                        config = config, 
+                        method = "parametric_si")
+  
+  res$I
+  
+  
+})
+
+# Compare reconstructed with the reported incidence
+
+compare_incid <- imap(forecast_weeks, function(week, index) {
+  
+  end <- as.Date(week)
+  start <- end - 41 # select 6 weeks of data
+  analysis_period <- seq.Date(from = as.Date(start),
+                              to = as.Date(end),
+                              by = 1)
+  
+  incid <- deaths_to_use[deaths_to_use$dates %in% analysis_period, c("dates", location)]
+  colnames(incid) <- c("dates", "reported_incid")
+
+  incid$reconstructed_incid <- recon_daily_inc[[index]]
+  
+  incid
+  
+})
+
+names(compare_incid) <- forecast_weeks
+compare_incid <- bind_rows(compare_incid, .id = "week_ending") %>% 
+  pivot_longer(cols = reported_incid:reconstructed_incid,
+               names_to = "incid_type",
+               values_to = "incid")
+
+ggplot(compare_incid) +
+  geom_point(aes(x = dates, y = incid, col = incid_type)) +
+  facet_wrap(~week_ending)
