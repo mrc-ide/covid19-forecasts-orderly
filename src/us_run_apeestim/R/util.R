fd9f7d8a895a2fba813fe8eb9e98b36193935f59
rebeccanash <r.nash@imperial.ac.uk>
2023-01-24 15:58:14 +0000

    correction for ts_to_incid

--- src/us_run_apeestim/R/util.R
+++ src/us_run_apeestim/R/util.R
@@ -7,7 +7,7 @@ ts_to_incid <- function(ts, date_col, case_col) {
   
   first_date <- min(ts[[date_col]])
   last_date <- max(ts[[date_col]])
-  if(!any(ts[[case_col]]%%1==0)){
+  if(any(ts[[case_col]]%%1!=0)){
    ts[[case_col]] <- round(ts[[case_col]])
   }
   x <- tidyr::uncount(ts, weights = ts[[case_col]])
