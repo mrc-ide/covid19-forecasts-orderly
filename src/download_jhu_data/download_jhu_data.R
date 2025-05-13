55c7cc519f17e334d1c6fee14b4e4ae058c95d13
rebeccanash <r.nash@imperial.ac.uk>
2025-05-13 18:37:17 +0100

    set up download_jhu_data for orderly2

--- src/download_jhu_data/download_jhu_data.R
+++ src/download_jhu_data/download_jhu_data.R
@@ -1,3 +1,13 @@
+orderly_artefact(
+  "Latest time series from JHU",
+  c(
+    "covid19_confirmed_US.csv",
+    "covid19_deaths_US.csv"
+  )
+)
+
+library(readr)
+
 cases <- read_csv(
   "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
 )
